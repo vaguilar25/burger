110fbef1fbde0c7d26c699fc172ca1cb4c2e9c42
vaguilar25 <vivian.aguilar@gmail.com>
2018-12-26 01:21:19 -0600

    add JAWSSQL

--- config/connection.js
+++ config/connection.js
@@ -4,23 +4,28 @@
 
 // Require mysql
 var mysql = require("mysql");
-
+var connection;
 // Set up our connection information
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection=mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "password",
+        database: "burgers_db"
+    });
+}
 
 // Connect to the database
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
 });
 
 // Export connection
